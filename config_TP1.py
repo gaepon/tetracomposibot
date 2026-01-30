0c3a39071af84275442818396ba6af3be0cc9775
Jules Rousseaux <jules.rousseaux@etu.sorbonne-universite.fr>
2026-01-30 10:20:05 +0100

    Did TP1, began wor on project with a smortBot

--- config_TP1.py
+++ config_TP1.py
@@ -21,10 +21,17 @@ display_time_stats = True
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
 import robot_dumb
+import robot_braitenberg_avoider as RAvoid
+import robot_braitenberg_loveWall as RWall
+import robot_braitenberg_hateWall as RAWall
+import robot_subsomption as RSub
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(RAvoid.Robot_player(4, y_center, 0, name="Sea Otter", team="The Otter Team"))
+    #robots.append(RWall.Robot_player(6, y_center, 0, name="River Otter", team="The Otter Team"))
+    robots.append(RAWall.Robot_player(8, y_center, 0, name="Otter Space", team="The Otter Team"))
+    robots.append(RSub.Robot_player(6, y_center, 0, name="Smort Otter", team="The Otter Team"))
     return robots
--- robot_challenger.py
+++ robot_challenger.py
@@ -1,7 +1,7 @@
 # Projet "robotique" IA&Jeux 2025
 #
 # Binome:
-#  Prénom Nom No_étudiant/e : _________
+#  Prénom Nom No_étudiant/e : Jules Rousseaux 21210789
 #  Prénom Nom No_étudiant/e : _________
 #
 # check robot.py for sensor naming convention
@@ -13,7 +13,7 @@ nb_robots = 0
 
 class Robot_player(Robot):
 
-    team_name = "Challenger"  # vous pouvez modifier le nom de votre équipe
+    team_name = "The Otter Team"  # vous pouvez modifier le nom de votre équipe
     robot_id = -1             # ne pas modifier. Permet de connaitre le numéro de votre robot.
     memory = 0                # vous n'avez le droit qu'a une case mémoire qui doit être obligatoirement un entier
 
@@ -23,8 +23,45 @@ class Robot_player(Robot):
         nb_robots+=1
         super().__init__(x_0, y_0, theta_0, name="Robot "+str(self.robot_id), team=self.team_name)
 
+    def hateFriends(self,sBot, sTeam):
+        pass
+
+    def hateWall(self, sWall):
+        return sWall[sensor_front]*.5+0.1, sWall[sensor_front_right]*-1.4+sWall[sensor_front_left]*1.4+0.01
+
+    def loveEnemyBot(self, sBot, sTeam):
+        return sBot[sensor_front]*.5+0.1, sBot[sensor_front_right]*1.4+sBot[sensor_front_left]*-1.4
+
+    def sub(self, sensors, sensor_view=None, sensor_to_robot=None, sensor_to_wall=None, sensor_robot=None, sensor_team=None):
+        if false:
+            translation, rotation = self.loveEnemyBot(sensor_to_robot, sensor_team)
+        elif false:
+            translation, rotation = self.loveEnemyBot(sensor_to_robot, sensor_team)
+        else:
+            translation, rotation = self.hateWall(sensor_to_wall)
+        return translation, rotation
+
+    def stay(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
+        return 0,0
+
     def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
-        translation = sensors[sensor_front]
-        rotation = 1.0 * sensors[sensor_front_left] - 1.0 * sensors[sensor_front_right] + (random.random()-0.5)*0.1
+
+        behaviours = [self.sub, self.stay, self.stay, self.stay]
+
+        sensor_to_wall = []
+        sensor_to_robot = []
+        for i in range (0,8):
+            if  sensor_view[i] == 1:
+                sensor_to_wall.append( sensors[i] )
+                sensor_to_robot.append(1.0)
+            elif  sensor_view[i] == 2:
+                sensor_to_wall.append( 1.0 )
+                sensor_to_robot.append( sensors[i] )
+            else:
+                sensor_to_wall.append(1.0)
+                sensor_to_robot.append(1.0)
+        
+        translation, rotation = behaviours[self.id](sensors, sensor_view, sensor_to_robot, sensor_to_wall, sensor_robot, sensor_team)
+
         return translation, rotation, False
 
